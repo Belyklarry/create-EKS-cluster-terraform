8b904c5abc41657ca13b4084efbb7157d189faf8
larry <larry.moyo9@gmail.com>
2022-07-26 12:59:31 +0200

    Fixed state lock

--- backend.tf
+++ backend.tf
@@ -3,7 +3,9 @@ terraform {
     bucket = "jenkins-eks-111"
     key    = "dev/"
     region = "us-east-1"
+
+    dynamodb_table = "jenkins-eks-tfstate-lock"
   }
 
-  dynamodb_table = "jenkins-eks-tfstate-lock"
+  
 }
